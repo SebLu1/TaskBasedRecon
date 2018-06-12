624369f7f5a64a04e4a558b438cb30462a831f08
Seb <sl767@cam.ac.uk>
2018-06-12 12:22:49 +0100

    joint training allows pretraining segmentation

--- JointTraining.py
+++ JointTraining.py
@@ -1,5 +1,8 @@
 from Framework import joint_training
 
 jt = joint_training()
-for k in range(3):
-    jt.pretrain_reconstruction(200)
+# for k in range(3):
+#     jt.pretrain_reconstruction(200)
+
+for k in range(5):
+    jt.pretrain_segmentation_true_input(500)
